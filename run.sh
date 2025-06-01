d651fc3181ea6ecec72f44ffda8fd1a876560dcd
Khoa Tran <ovn6002@gmail.com>
2025-06-01 23:33:39 +0700

    update run script

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+set -e
+
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+fi
+source venv/bin/activate
+
+pip install -r requirements.txt
+
 ansible-playbook -i inventory.ini configure_ubuntu_vms.yml \
     --private-key ~/.ssh/ansible_id_ed25519 \
     --ask-vault-pass
